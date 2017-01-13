c07e8f2bd56ac166c2f0504ce514d1c337a72e08
robobot97 <arjunkhurana26@hotmail.com>
2017-01-13 21:55:44 +0000

    Connected mailer to the website

--- controller/controller_signup.php
+++ controller/controller_signup.php
@@ -10,6 +10,9 @@ include_once('../model/model_signup.php');
 // add user/pw to database (with hash in safe version)
 add_user($_POST['username'], $_POST['password']);
 
+//sends email to user
+sendEmail($_GET[username])
+
 // store the username during this session, and redirect to home page.
 $_SESSION['current_user'] = $_POST['username'];
 echo "<a href='../view/view_home.php'>Go to home page</a>";
--- model/model_signup.php
+++ model/model_signup.php
@@ -17,6 +17,41 @@ function add_user ($username, $password){
     }
 }
 
+function sendEmail($emailTo){
+
+	require_once 'PHPMailer/PHPMailerAutoload.php';
+	
+	date_default_timezone_set('Etc/UTC');
+	$mail = new PHPMailer;
+	
+	//$mail->SMTPDebug = 0;
+
+	$mail->isSMTP();
+	$mail->Debugoutput = 'html';
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = "hack.me.email.enquiry@gmail.com";
+	$mail->Password = "hackme123";
+	$mail->SMTPSecure = 'tls';
+	$mail->Port = 587;
+	
+	$mail->isHTML(true);
+
+	$mail->setFrom("hack.me.email.enquiry@gmail.com", 'H4ck.me');
+	$mail->addAddress($emailTo);
+	$mail->Subject = 'Welcome to H4ck.me';
+
+	$mail->Body = "Thank you for using <b>H4ck.me</b>. We hope you will enjoy it. :D";
+
+
+	if(!$mail->send()) {
+    	echo 'Message could not be sent.';
+	    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+    	echo 'Message has been sent';
+	}
+}
+
 echo "model_signup included"."<br />";
 
  ?>
