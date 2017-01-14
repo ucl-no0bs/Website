8718c6740060380736b2f97812731f7715a5840e
Aksel <akselcakmak@gmail.com>
2017-01-14 00:58:48 +0000

    added some explanations on how to perform the example CSRF attack

--- controller/controller_home_addSnippet.php
+++ controller/controller_home_addSnippet.php
@@ -8,6 +8,10 @@ include_once('../model/model_home.php');
 
 add_snippet($_SESSION['current_user'], $_POST['snippet'], $_POST['isPublic']);
 
+// How to simulate the CSRF vulnerability:
+// You add a snippet with this JS script: window.open('http://localhost:8888/Scenario3/Bad-Guys/'), or you just click on the button created for the simulation.
+//Note that the attacker's website should be in a separate directory (to prove that our hack is indeed a CSRF attack")
+
 
 //echo "<script> window.location.assign('../view/view_home.php'); </script>";
 echo "<a href='../view/view_home.php'>Go to home page</a>";
